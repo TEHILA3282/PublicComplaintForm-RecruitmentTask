9088f8b9b593f9755988e3e79035cd2d1e61ac03
TEHILA3282 <TE3282@GMAIL.COM>
2026-07-12 02:57:05 +0300

    Complete recruitment task

---
+++ PublicComplaintForm_API/Models/MonthlyInquiryReport.cs
@@ -0,0 +1,21 @@
+﻿namespace PublicComplaintForm_API.Models
+{
+    public class MonthlyInquiryReport
+    {
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; } = string.Empty;
+
+        public int CurrentMonthCount { get; set; }
+
+        public int PreviousMonthCount { get; set; }
+
+        public int SameMonthLastYearCount { get; set; }
+
+        public int DifferenceFromPreviousMonth =>
+            CurrentMonthCount - PreviousMonthCount;
+
+        public int DifferenceFromLastYear =>
+            CurrentMonthCount - SameMonthLastYearCount;
+    }
+}
---
+++ PublicComplaintForm_API/Services/MonthlyInquiryReportService.cs
@@ -0,0 +1,46 @@
+﻿using PublicComplaintForm_API.Models;
+
+namespace PublicComplaintForm_API.Services
+{
+    public class MonthlyInquiryReportService
+    {
+        public List<MonthlyInquiryReport> GetMonthlyReport(DateTime reportMonth)
+        {
+            return new List<MonthlyInquiryReport>
+            {
+                new MonthlyInquiryReport
+                {
+                    DepartmentId = 1,
+                    DepartmentName = "מחלקת פניות הציבור",
+                    CurrentMonthCount = 125,
+                    PreviousMonthCount = 110,
+                    SameMonthLastYearCount = 98
+                },
+                new MonthlyInquiryReport
+                {
+                    DepartmentId = 2,
+                    DepartmentName = "מחלקת מידע",
+                    CurrentMonthCount = 84,
+                    PreviousMonthCount = 91,
+                    SameMonthLastYearCount = 79
+                },
+                new MonthlyInquiryReport
+                {
+                    DepartmentId = 3,
+                    DepartmentName = "מחלקת שירות",
+                    CurrentMonthCount = 142,
+                    PreviousMonthCount = 130,
+                    SameMonthLastYearCount = 121
+                },
+                new MonthlyInquiryReport
+                {
+                    DepartmentId = 4,
+                    DepartmentName = "מחלקת גבייה",
+                    CurrentMonthCount = 0,
+                    PreviousMonthCount = 8,
+                    SameMonthLastYearCount = 5
+                }
+            };
+        }
+    }
+}
